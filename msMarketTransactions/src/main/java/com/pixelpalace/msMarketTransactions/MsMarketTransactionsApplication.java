f19ef56fcb82fa1cc505c6fdca70bca0b4aa2444
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2023-10-20 16:38:07 -0300

    add: initial repo :tada:

---
+++ msMarketTransactions/src/main/java/com/pixelpalace/msMarketTransactions/MsMarketTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.pixelpalace.msMarketTransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMarketTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMarketTransactionsApplication.class, args);
+	}
+
+}
---
+++ msMarketTransactions/src/test/java/com/pixelpalace/msMarketTransactions/MsMarketTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelpalace.msMarketTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMarketTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
