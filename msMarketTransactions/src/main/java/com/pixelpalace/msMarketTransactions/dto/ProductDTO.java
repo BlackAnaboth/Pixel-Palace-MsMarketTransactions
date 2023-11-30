819955ab901bf4b922b09acb6d98f820fb2cd007
ornequarra <orne_sq31@hotmail.com>
2023-11-29 23:10:35 -0300

    fix snake case

--- msMarketTransactions/src/main/java/com/pixelpalace/msMarketTransactions/dto/ProductDTO.java
+++ msMarketTransactions/src/main/java/com/pixelpalace/msMarketTransactions/dto/ProductDTO.java
@@ -1,15 +1,19 @@
 package com.pixelpalace.msMarketTransactions.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.List;
 
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class ProductDTO {
 
     private Long id;
@@ -21,4 +25,5 @@ public class ProductDTO {
     private Double score;
     private List<String> imageUrl;
     private Double stock;
+    private Date releaseDate;
 }
